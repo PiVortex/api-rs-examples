105a0083964cb930e99869a53306093a542036a7
PiVortex <owengeorgehassall@gmail.com>
2024-11-28 10:30:04 +0000

    complete signers

---
+++ examples/accounts.rs
@@ -0,0 +1,87 @@
+use near_api::prelude::*;
+use near_crypto::SecretKey;
+use std::str::FromStr;
+
+#[tokio::main]
+async fn main() {
+    let network = NetworkConfig::testnet();
+
+    // Specify account Id
+    let my_account_id: AccountId = "a-whole-new-account.testnet".parse().unwrap();
+
+    // Create signer object from private key
+    // Need to use near_crypto::SecretKey
+    let private_key = SecretKey::from_str("ed25519:3bUTUXCPHPbAD5JDukzsWT5BaJ9iZA3FF9wLgYgRvzC7CDYMgmEExtxyGjnGATvmM3oggqUErvRkN9sjzNTD8yd7").unwrap();
+    let signer = Signer::new(Signer::secret_key(private_key)).unwrap();
+
+    // Create account in a standard way, save the seed phrase to a file
+    let new_account_id_1: AccountId = "some-account-sdgfj.testnet".parse().unwrap();
+
+    let res = Account::create_account()
+        .fund_myself(
+            new_account_id_1.clone(),
+            my_account_id.clone(),
+            NearToken::from_near(1),
+        ) 
+        .new_keypair()
+        .save_generated_seed_to_file("./new_account_seed".into())
+        .unwrap()
+        .with_signer(signer.clone())
+        .send_to(&network)
+        .await
+        .unwrap();
+
+    // // Create an account with a load of custom stuff
+    // let new_account_id_2: AccountId = "some-account-s4figwe.testnet".parse().unwrap();
+
+    // let (seed_phrase, res) = Account::create_account()
+    //     .fund_myself(
+    //         new_account_id_2.clone(),
+    //         my_account_id.clone(),
+    //         NearToken::from_near(1),
+    //     )
+    //     .new_keypair()
+    //     .word_count(24)
+    //     .generate_seed_phrase()
+    //     .unwrap();
+
+    // let res = res
+    //     .with_signer(signer.clone())
+    //     .send_to(&network)
+    //     .await
+    //     .unwrap();
+
+    // let new_account_id_3: AccountId = "subac.a-whole-new-account.testnet".parse().unwrap();
+
+    // // Create sub account
+    // let res = Account::create_account()
+    //     .fund_myself(
+    //         new_account_id_3.clone(),
+    //         my_account_id.clone(),
+    //         NearToken::from_near(1),
+    //     ) 
+    //     .new_keypair()
+    //     .save_generated_seed_to_file("./new_account_seed".into())
+    //     .unwrap()
+    //     .with_signer(signer.clone())
+    //     .send_to(&network)
+    //     .await
+    //     .unwrap();
+
+    // // Delete account
+    // Account::delete_account(new_account_id_1.clone())
+    //     .with_signer(signer.clone())
+    //     .send_to(&network)
+    //     .await
+    //     .unwrap();
+}
+
+// A lot more can be done here
+// hd path
+// passphrase
+// use seed phrase
+// use public key
+// use public key from
+// generate secret key
+// sponsor by faucet
+// create implicit 
--- examples/signers.rs
+++ examples/signers.rs
@@ -1,7 +1,10 @@
 use near_api::prelude::*;
-use near_crypto::SecretKey;
+use near_api::signer::keystore::KeystoreSigner;
+use near_crypto::{SecretKey, PublicKey};
 use near_primitives::account::AccessKeyPermission;
 use std::str::FromStr;
+use std::path::Path;
+use std::env;
 
 #[tokio::main]
 async fn main() {
@@ -26,7 +29,71 @@ async fn main() {
         .unwrap();
 
     // access key file
-    // let file_signer = Signer::new(Signer::access_keyfile(./))
+    let home_dir = env::var("HOME").unwrap();
+    let credentials_dir = Path::new(&home_dir).join(".near-credentials");
+    let file_path = credentials_dir.join(format!("testnet/{}.json", my_account_id));
+    let file_signer = Signer::new(Signer::access_keyfile(file_path).unwrap()).unwrap();
+
+    // test file signer
+    Account(my_account_id.clone())
+        .add_key(AccessKeyPermission::FullAccess)
+        .new_keypair()
+        .generate_secret_key()
+        .unwrap()
+        .1
+        .with_signer(file_signer.clone())
+        .send_to(&network)
+        .await
+        .unwrap();
+
+    // keystore specifying public key
+    let public_key = PublicKey::from_str("ed25519:G5k73gFDHjFQTaHp26zShbcMt2Quth2sJJfqra1eetuT").unwrap();
+    let keystore_signer = Signer::new(Signer::keystore(public_key)).unwrap();
+
+    // test keystore signer
+    Account(my_account_id.clone())
+        .add_key(AccessKeyPermission::FullAccess)
+        .new_keypair()
+        .generate_secret_key()
+        .unwrap()
+        .1
+        .with_signer(keystore_signer.clone())
+        .send_to(&network)
+        .await
+        .unwrap();
+
+    // Keystore not specifying public key
+    let search_keystore_signer = KeystoreSigner::search_for_keys(my_account_id.clone(), &network).await.unwrap();
+    let keystore_signer_search = Signer::new(search_keystore_signer).unwrap();
+
+    // test keystore signer search
+    Account(my_account_id.clone())
+        .add_key(AccessKeyPermission::FullAccess)
+        .new_keypair()
+        .generate_secret_key()
+        .unwrap()
+        .1
+        .with_signer(keystore_signer_search.clone())
+        .send_to(&network)
+        .await
+        .unwrap();
+
+    // using seed phrase
+    let seed_phrase = "shoe sell gate jelly half tissue parrot robust census lens staff ship".to_string();
+    let seed_phrase_signer = Signer::new(Signer::seed_phrase(seed_phrase, None).unwrap()).unwrap();
+
+    // test seed phrase signer
+    Account(my_account_id.clone())
+        .add_key(AccessKeyPermission::FullAccess)
+        .new_keypair()
+        .generate_secret_key()
+        .unwrap()
+        .1
+        .with_signer(seed_phrase_signer.clone())
+        .send_to(&network)
+        .await
+        .unwrap();
 }
 
 // Leave out ledger, not common + don't want to attach my own!
+// There is also seed_phrase_with_hd_path, also this and seed_phrase have the options for password
